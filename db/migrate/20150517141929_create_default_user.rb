08e003521445ccda351bc65b3b4001ec2e31b3f1
xinaan <cnatheboy@gmail.com>
2015-05-18 15:21:13 +0500

    asset

--- db/migrate/20150517141929_create_default_user.rb
+++ db/migrate/20150517141929_create_default_user.rb
@@ -1,13 +1,10 @@
 class CreateDefaultUser < ActiveRecord::Migration
-  def change
-	create_default 
+  def up
+	user = User.create( username: 'admin', password: '123@abc', password_confirmation: '123@abc')
   end
 
-  def self.create_default
-	adminUser = User.new
-	adminUser.username = "admin"
-	adminUser.hashed_password = "26ce0f100f0bf653cc2e4fbf3592af83a1e86cdf"
-	adminUser.salt = "97e3a6902f64c2bea217ef76014edc81c1227678"
-	adminUser.save
+  def down
+	user = User.find_by_username( 'admin' )
+    user.destroy
   end
 end
